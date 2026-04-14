cbc241dad71225c5a9b5160805f104a82863d823
saranyaadineni <asaranya@speshway.com>
2026-04-14 18:13:00 +0530

    hii

--- backend/index.js
+++ backend/index.js
@@ -89,6 +89,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://bikes-ay5i.vercel.app",
+  "https://bikes-green.vercel.app",
   process.env.PUBLIC_BASE_URL
 ].filter(Boolean);
 
